3a1fcc7b900b9a04202b2982be7d27048faebd1b
hmadhi <hmadhi@vodamail.co.za>
2021-08-31 12:12:22 +0200

    complete Dispense details

--- Optimed/src/main/java/com/madhis/optimed/controller/DispenseController.java
+++ Optimed/src/main/java/com/madhis/optimed/controller/DispenseController.java
@@ -2,8 +2,10 @@ package com.madhis.optimed.controller;
 
 import com.madhis.optimed.entity.Consult;
 import com.madhis.optimed.entity.Dispense;
+import com.madhis.optimed.entity.Patient;
 import com.madhis.optimed.service.ConsultService;
 import com.madhis.optimed.service.DispenseService;
+import com.madhis.optimed.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,22 +17,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class DispenseController {
 
-
+	@Autowired
+	private PatientService patientService;
+	
 	@Autowired
 	private DispenseService dispenseService;
 	
 	@Autowired
 	private ConsultService consultService;
 	
-	@RequestMapping(value="/dispense/{id}",method = {RequestMethod.POST})
-        public String dispenseForm(Model model, @ModelAttribute("dispense") Dispense dispense, @PathVariable(value="id") Long consultId){
-		System.out.println("consultId = " + consultId);
+	
+	@RequestMapping(value="/patient/{pid}/consult/{cid}",method = {RequestMethod.POST})
+        public String dispenseForm(Model model, @ModelAttribute("dispense") Dispense dispense,@PathVariable(value="pid") Long patientId, @PathVariable(value="cid") Long consultId){
+		Patient patient = patientService.findPatientById(patientId);
 		Consult consult = consultService.findByConsultId(consultId);
 		model.addAttribute("consult",consult);
-		
+		model.addAttribute("patient",patient);
 		consult.getDispenses().add(dispense);
 		dispenseService.addDispense(dispense);
 		
-		return "index";
+		return "dispense";
 	} 
 }
