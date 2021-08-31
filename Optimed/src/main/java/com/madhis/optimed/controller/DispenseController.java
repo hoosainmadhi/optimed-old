2d6ad4ff51433789c4cc9bbdb1db5e8a5346f7a6
hmadhi <hmadhi@vodamail.co.za>
2021-08-31 13:05:27 +0200

    add link for dispenseDetails on consultForm

--- Optimed/src/main/java/com/madhis/optimed/controller/DispenseController.java
+++ Optimed/src/main/java/com/madhis/optimed/controller/DispenseController.java
@@ -6,6 +6,7 @@ import com.madhis.optimed.entity.Patient;
 import com.madhis.optimed.service.ConsultService;
 import com.madhis.optimed.service.DispenseService;
 import com.madhis.optimed.service.PatientService;
+import javax.security.auth.message.callback.PrivateKeyCallback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,15 +28,18 @@ public class DispenseController {
 	private ConsultService consultService;
 	
 	
-	@RequestMapping(value="/patient/{pid}/consult/{cid}",method = {RequestMethod.POST})
+//	@RequestMapping(value="/patient/{pid}/consult/{cid}",method = {RequestMethod.POST})
+	@RequestMapping(value="/patient/{pid}/consult/{cid}")
         public String dispenseForm(Model model, @ModelAttribute("dispense") Dispense dispense,@PathVariable(value="pid") Long patientId, @PathVariable(value="cid") Long consultId){
 		Patient patient = patientService.findPatientById(patientId);
 		Consult consult = consultService.findByConsultId(consultId);
+		
 		model.addAttribute("consult",consult);
 		model.addAttribute("patient",patient);
-		consult.getDispenses().add(dispense);
-		dispenseService.addDispense(dispense);
-		
+		if(dispense.getDispenseItem() != null){
+		    consult.getDispenses().add(dispense);
+		    dispenseService.addDispense(dispense);
+		}
 		return "dispense";
 	} 
 }
